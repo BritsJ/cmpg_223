6167c3e91b9a847d1ebbab16c5b232fdd2363157
Jaco Brits <j.brits@hotmail.com>
2024-08-12 20:14:21 +0200

    Login
    Add Login screen

---
+++ Application/CMPG223_Project/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+ * Login page of the application.
+ * You can login using username "admin" and password "admin"
+ */
+
+namespace Exam_Brits_JJ_38980274
+{
+    public partial class Login : Form
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Username = txtUsername.Text;
+            Password = txtPassword.Text;
+            this.Close();
+
+        }
+    }
+}
