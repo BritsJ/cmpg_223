3c7aec2e158128487271d076f41633b484315361
Jaco Brits <j.brits@hotmail.com>
2024-08-16 07:51:41 +0200

    Add Sale Screen

---
+++ Application/CMPG223_Project/frmSalePay.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CMPG223_Project
+{
+    partial class frmSalePopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSalePopup));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cash:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(209, 99);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(224, 38);
+            this.txtAmount.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Change:";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(51, 153);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(156, 33);
+            this.btnCalc.TabIndex = 7;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(209, 44);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(224, 38);
+            this.txtTotal.TabIndex = 8;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChange.Location = new System.Drawing.Point(209, 201);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.ReadOnly = true;
+            this.txtChange.Size = new System.Drawing.Size(224, 38);
+            this.txtChange.TabIndex = 9;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(51, 253);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(156, 33);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Done";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmSalePopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 308);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtChange);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmSalePopup";
+            this.Text = "Pay";
+            this.Load += new System.EventHandler(this.frmSalePopup_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtChange;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
--- database/sql_scripts/insert_data.sql
+++ database/sql_scripts/insert_data.sql
@@ -2,3 +2,53 @@ INSERT INTO [SYSTEM_USER] (Username, PasswordHash, IsAdmin, IsActive)
 VALUES 
 ('test', 'test', 0, 1),
 ('admin', 'admin', 1, 1);
+
+-- Insert records into CATEGORY
+INSERT INTO [dbo].[CATEGORY] (Category_Code, Category_Name, Category_Description, Is_Active)
+VALUES ('ELEC', 'Electrical Goods', 'Category for electrical goods like switches and plugs', 1),
+       ('LIGHT', 'Lighting', 'Category for lighting products', 1);
+
+-- Insert records into SUBCATEGORY
+INSERT INTO [dbo].[SUBCATEGORY] (Category_Id, Subcategory_Code, Subcategory_Name, Subcategory_Description, Is_Active)
+VALUES ((SELECT Category_Id FROM [dbo].[CATEGORY] WHERE Category_Code = 'ELEC'), 'SWITCH', 'Switches', 'Subcategory for light switches', 1),
+       ((SELECT Category_Id FROM [dbo].[CATEGORY] WHERE Category_Code = 'LIGHT'), 'BULB', 'Bulbs', 'Subcategory for light bulbs', 1);
+
+-- Insert records into CLIENT
+INSERT INTO [dbo].[CLIENT] (Client_Code, Contact_Person_Name, Business_Name, Phone_Number, Email_Address, Physical_Address, Additional_Info, Is_Active)
+VALUES (1001, 'John Doe', 'Electrical Supplies Inc.', '123-456-7890', 'johndoe@electricalsupplies.com', '123 Electric Avenue', 'Frequent Buyer', 1),
+       (1002, 'Jane Smith', 'Lighting Solutions', '987-654-3210', 'janesmith@lightingsolutions.com', '456 Bright Street', 'Preferred Client', 1);
+
+-- Insert records into EMPLOYEE
+INSERT INTO [dbo].[EMPLOYEE] (Id_Number, First_Name, Middle_Name, Last_Name, Hire_Date, Phone_Number, Email_Address, Physical_Address)
+VALUES ('EMP001', 'Michael', 'T.', 'Johnson', '2023-01-15', '555-123-4567', 'mjohnson@electricalstore.com', '789 Main Street'),
+       ('EMP002', 'Emily', 'R.', 'Davis', '2023-02-20', '555-987-6543', 'edavis@electricalstore.com', '321 Oak Avenue');
+
+-- Insert records into EQUIPMENT
+INSERT INTO [dbo].[EQUIPMENT] (Equipment_Code, Name, Description, Quantity, Quantity_Checked_Out, Is_Active)
+VALUES (2001, 'Voltage Tester', 'Used to test electrical circuits', 15, 3, 1),
+       (2002, 'Wire Stripper', 'Used to strip electrical wires', 25, 5, 1);
+
+-- Insert records into STOCK
+INSERT INTO [dbo].[STOCK] (Subcategory_Id, Stock_Code, Stock_Name, Stock_Description, Purchase_Date, Purchase_Price, Selling_Price, Quantity)
+VALUES ((SELECT Subcategory_Id FROM [dbo].[SUBCATEGORY] WHERE Subcategory_Code = 'SWITCH'), 'SW001', 'Light Switch', 'Standard light switch', '2024-08-01', 3.00, 5.00, 100),
+       ((SELECT Subcategory_Id FROM [dbo].[SUBCATEGORY] WHERE Subcategory_Code = 'BULB'), 'BULB001', 'LED Bulb', 'Energy-saving LED bulb', '2024-08-05', 1.50, 3.00, 200);
+
+-- Insert records into JOB
+INSERT INTO [dbo].[JOB] (Employee_Number, Client_Id, Start_Date_Time, End_Date_Time, Job_Description)
+VALUES ((SELECT Employee_Number FROM [dbo].[EMPLOYEE] WHERE Id_Number = 'EMP001'), (SELECT Client_Id FROM [dbo].[CLIENT] WHERE Client_Code = 1001), '2024-08-10 09:00', '2024-08-10 17:00', 'Installation of light switches'),
+       ((SELECT Employee_Number FROM [dbo].[EMPLOYEE] WHERE Id_Number = 'EMP002'), (SELECT Client_Id FROM [dbo].[CLIENT] WHERE Client_Code = 1002), '2024-08-11 10:00', NULL, 'Lighting consultation');
+
+-- Insert records into JOB_EQUIPMENT
+INSERT INTO [dbo].[JOB_EQUIPMENT] (Job_Id, Equipment_Id)
+VALUES ((SELECT Job_Id FROM [dbo].[JOB] WHERE Job_Description = 'Installation of light switches'), (SELECT Equipment_Id FROM [dbo].[EQUIPMENT] WHERE Equipment_Code = 2001)),
+       ((SELECT Job_Id FROM [dbo].[JOB] WHERE Job_Description = 'Lighting consultation'), (SELECT Equipment_Id FROM [dbo].[EQUIPMENT] WHERE Equipment_Code = 2002));
+
+-- Insert records into SALE
+INSERT INTO [dbo].[SALE] (Employee_Number, Sale_Date_Time, Cash_Received)
+VALUES ((SELECT Employee_Number FROM [dbo].[EMPLOYEE] WHERE Id_Number = 'EMP001'), '2024-08-12 14:00', 500.00),
+       ((SELECT Employee_Number FROM [dbo].[EMPLOYEE] WHERE Id_Number = 'EMP002'), '2024-08-13 11:30', 600.00);
+
+-- Insert records into SALE_ITEM
+INSERT INTO [dbo].[SALE_ITEM] (Stock_Id, Sale_Id, Quantity, Price)
+VALUES ((SELECT Stock_Id FROM [dbo].[STOCK] WHERE Stock_Code = 'SW001'), (SELECT Sale_Id FROM [dbo].[SALE] WHERE Sale_Date_Time = '2024-08-12 14:00'), 50, 250.00),
+       ((SELECT Stock_Id FROM [dbo].[STOCK] WHERE Stock_Code = 'BULB001'), (SELECT Sale_Id FROM [dbo].[SALE] WHERE Sale_Date_Time = '2024-08-13 11:30'), 100, 300.00);
